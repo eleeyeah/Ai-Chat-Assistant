58bee6ebc47776bfa93840af509bcc131408d551
eleeyeah <ilietmks@gmail.com>
2023-05-25 16:22:07 +0200

    chat input

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,28 +1,38 @@
+'use client'
+
+import { FC } from 'react'
+import ChatInput from './ChatInput'
+import ChatHeader from './ChatHeader'
 import {
   Accordion,
+  AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "@/components/ui/accordion";
-import { FC } from "react";
+} from './ui/accordion'
 
 const Chat: FC = () => {
   return (
     <Accordion
-      type="single"
+      type='single'
       collapsible
-      className="relativ bg-white z-40 shadow"
-    >
-      <AccordionItem value="item-1">
-        <div className="fixed right-8 w-80 bottom-8 bg-white border border-gray-200 rounded-md overflow-hidden">
-          <div className="w-full h-full flex flex-col">
-            <AccordionTrigger className="px-6 border-b border-zinc-300">
+      className='relative bg-white z-40 shadow'>
+      <AccordionItem value='item-1'>
+        <div className='fixed right-8 w-80 bottom-8 bg-white border border-gray-200 rounded-md overflow-hidden'>
+          <div className='w-full h-full flex flex-col'>
+            <AccordionTrigger className='px-6 border-b border-zinc-300'>
               <ChatHeader />
             </AccordionTrigger>
+            <AccordionContent>
+              <div className='flex flex-col h-80'>
+                {/* <ChatMessages className='px-2 py-3 flex-1' /> */}
+                <ChatInput className='px-4' />
+              </div>
+            </AccordionContent>
           </div>
         </div>
       </AccordionItem>
     </Accordion>
-  );
-};
+  )
+}
 
-export default Chat;
+export default Chat
